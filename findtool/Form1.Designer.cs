659e741f66ff37bf71aedec40d47f21fd656658a
info <info@presto.or.jp>
2019-02-27 15:37:15 +0900

    プロジェクト ファイルを追加します。

---
+++ findtool/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace findtool
+{
+    partial class findTool
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Directory = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.directoryBox = new System.Windows.Forms.TextBox();
+            this.findstringBox = new System.Windows.Forms.TextBox();
+            this.dirbutton = new System.Windows.Forms.Button();
+            this.findbutton = new System.Windows.Forms.Button();
+            this.LUcheckBox = new System.Windows.Forms.CheckBox();
+            this.subDirectorycheckBox = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.outtextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Directory
+            // 
+            this.Directory.AutoSize = true;
+            this.Directory.Location = new System.Drawing.Point(18, 30);
+            this.Directory.Name = "Directory";
+            this.Directory.Size = new System.Drawing.Size(52, 12);
+            this.Directory.TabIndex = 0;
+            this.Directory.Text = "Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "検索文字";
+            // 
+            // directoryBox
+            // 
+            this.directoryBox.Location = new System.Drawing.Point(142, 32);
+            this.directoryBox.Name = "directoryBox";
+            this.directoryBox.Size = new System.Drawing.Size(776, 19);
+            this.directoryBox.TabIndex = 2;
+            // 
+            // findstringBox
+            // 
+            this.findstringBox.Location = new System.Drawing.Point(142, 89);
+            this.findstringBox.Name = "findstringBox";
+            this.findstringBox.Size = new System.Drawing.Size(294, 19);
+            this.findstringBox.TabIndex = 3;
+            // 
+            // dirbutton
+            // 
+            this.dirbutton.Location = new System.Drawing.Point(88, 27);
+            this.dirbutton.Name = "dirbutton";
+            this.dirbutton.Size = new System.Drawing.Size(48, 24);
+            this.dirbutton.TabIndex = 4;
+            this.dirbutton.Text = "...";
+            this.dirbutton.UseVisualStyleBackColor = true;
+            this.dirbutton.Click += new System.EventHandler(this.dirbutton_Click);
+            // 
+            // findbutton
+            // 
+            this.findbutton.Location = new System.Drawing.Point(90, 59);
+            this.findbutton.Name = "findbutton";
+            this.findbutton.Size = new System.Drawing.Size(48, 24);
+            this.findbutton.TabIndex = 5;
+            this.findbutton.Text = "検索";
+            this.findbutton.UseVisualStyleBackColor = true;
+            this.findbutton.Click += new System.EventHandler(this.findbutton_Click);
+            // 
+            // LUcheckBox
+            // 
+            this.LUcheckBox.AutoSize = true;
+            this.LUcheckBox.Checked = true;
+            this.LUcheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.LUcheckBox.Location = new System.Drawing.Point(148, 64);
+            this.LUcheckBox.Name = "LUcheckBox";
+            this.LUcheckBox.Size = new System.Drawing.Size(115, 16);
+            this.LUcheckBox.TabIndex = 7;
+            this.LUcheckBox.Text = "文字の大小を区別";
+            this.LUcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // subDirectorycheckBox
+            // 
+            this.subDirectorycheckBox.AutoSize = true;
+            this.subDirectorycheckBox.Location = new System.Drawing.Point(269, 64);
+            this.subDirectorycheckBox.Name = "subDirectorycheckBox";
+            this.subDirectorycheckBox.Size = new System.Drawing.Size(92, 16);
+            this.subDirectorycheckBox.TabIndex = 11;
+            this.subDirectorycheckBox.Text = "サブディレクトリ";
+            this.subDirectorycheckBox.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(487, 59);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(431, 45);
+            this.flowLayoutPanel1.TabIndex = 17;
+            // 
+            // outtextBox
+            // 
+            this.outtextBox.Location = new System.Drawing.Point(20, 114);
+            this.outtextBox.Multiline = true;
+            this.outtextBox.Name = "outtextBox";
+            this.outtextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outtextBox.Size = new System.Drawing.Size(898, 375);
+            this.outtextBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 24);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "ファイル名検索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // findTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 501);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.subDirectorycheckBox);
+            this.Controls.Add(this.LUcheckBox);
+            this.Controls.Add(this.outtextBox);
+            this.Controls.Add(this.findbutton);
+            this.Controls.Add(this.dirbutton);
+            this.Controls.Add(this.findstringBox);
+            this.Controls.Add(this.directoryBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Directory);
+            this.Name = "findTool";
+            this.Text = "FindTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Directory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox directoryBox;
+        private System.Windows.Forms.TextBox findstringBox;
+        private System.Windows.Forms.Button dirbutton;
+        private System.Windows.Forms.Button findbutton;
+        private System.Windows.Forms.CheckBox LUcheckBox;
+        private System.Windows.Forms.CheckBox subDirectorycheckBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox outtextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ findtool/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace findtool
+{
+    public partial class findTool : Form
+    {
+        public findTool()
+        {
+            InitializeComponent();
+            directoryBox.Text = Properties.Settings.Default.path;
+            string[] exts = new string[] { "cs", "cpp","c", "h", "hpp", "txt", "php", "js", "css", "config", "xml", "json" };
+            foreach (string txt in exts)
+            {
+                flowLayoutPanel1.Controls.Add(new CheckBox() { Text = txt, Checked = true , Width=60, Height=18});
+            }
+        }
+
+        private void dirbutton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dirDia = new FolderBrowserDialog();
+            if (directoryBox.Text != "") {
+                dirDia.SelectedPath = directoryBox.Text;
+            }
+            else
+            {
+                dirDia.SelectedPath = @"C:\Users\sugiy\Local Sites";
+            }
+
+            if (dirDia.ShowDialog() == DialogResult.OK)
+            {
+                directoryBox.Text = dirDia.SelectedPath;
+                Properties.Settings.Default.path = directoryBox.Text;
+                Properties.Settings.Default.Save();
+            }
+
+        }
+
+        private void findbutton_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            string toppath = directoryBox.Text;
+            outtextBox.Text = "";
+            //指定したディレクトリのファイルをすべて取得する
+            DirectoryInfo di = new DirectoryInfo(toppath);
+//            outtextBox.Text += toppath + Environment.NewLine;
+            findstring(di);
+
+            //サブディレクトリのファイルをすべて取得する
+            IEnumerable<DirectoryInfo> subFolders = di.EnumerateDirectories("*", SearchOption.AllDirectories);
+            //サブフォルダを列挙する
+            foreach (DirectoryInfo subFolder in subFolders)
+            {
+                findstring(subFolder);
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void findstring(DirectoryInfo di)
+        {
+            List<String> patterns = new List<string>();
+            foreach (CheckBox ext in flowLayoutPanel1.Controls)
+            {
+                if (ext.Checked) patterns.Add("*." + ext.Text);
+            }
+            // php js cssで3回行う
+            foreach (string pattern in patterns)
+            {
+                FileInfo[] files = di.GetFiles(pattern, SearchOption.TopDirectoryOnly);
+                if (subDirectorycheckBox.Checked)
+                {
+                    files = di.GetFiles(pattern, SearchOption.AllDirectories);
+                }
+                StringBuilder sb = new StringBuilder();
+                foreach (FileInfo f in files)
+                {
+                    if (f.Name.Contains("min.js")) continue; 
+                    //"C:\test\1.txt"をShift-JISコードとして開く
+                    StreamReader sr = new System.IO.StreamReader(f.FullName);
+                    //内容を一行ずつ読み込む
+                    int row = 1;
+                    bool first = true;
+                    while (sr.Peek() > -1)
+                    {
+                        string line = sr.ReadLine();
+                        if (LUcheckBox.Checked)
+                        {
+                            if (line.Contains(findstringBox.Text))
+                            {
+                                if (first)
+                                {
+                                    sb.Append(Environment.NewLine);
+                                    sb.Append(f.Name);
+                                    sb.Append(Environment.NewLine);
+                                    first = false;
+                                }
+                                sb.Append(row.ToString() + ": " + line);
+                                sb.Append(Environment.NewLine);
+                            }
+                        }
+                        else
+                        {
+                            if (line.ToLower().Contains(findstringBox.Text.ToLower()))
+                            {
+                                if (first)
+                                {
+                                    sb.Append(f.Name);
+                                    sb.Append(Environment.NewLine);
+                                    first = false;
+                                }
+                                sb.Append(row.ToString() + ": " + line);
+                                sb.Append(Environment.NewLine);
+                            }
+                        }
+                        row++;
+                    }
+                    //閉じる
+                    sr.Close();
+                }
+                if (sb.Length > 1)
+                {
+                    outtextBox.Text += Environment.NewLine + Environment.NewLine + di.FullName + Environment.NewLine;
+                }
+                outtextBox.Text += sb.ToString();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            string toppath = directoryBox.Text;
+            outtextBox.Text = "";
+            //指定したディレクトリのファイルをすべて取得する
+            DirectoryInfo di = new DirectoryInfo(toppath);
+            findfiles(di);
+
+            //サブディレクトリのファイルをすべて取得する
+            IEnumerable<DirectoryInfo> subFolders = di.EnumerateDirectories("*", SearchOption.AllDirectories);
+            //サブフォルダを列挙する
+            foreach (DirectoryInfo subFolder in subFolders)
+            {
+                findfiles(subFolder);
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void findfiles(DirectoryInfo di)
+        {
+            FileInfo[] files = di.GetFiles();
+            StringBuilder sb = new StringBuilder();
+            foreach (FileInfo f in files)
+            {
+                if (f.Name.Contains(findstringBox.Text))
+                {
+                    sb.Append(f.FullName + Environment.NewLine);
+                }
+            }
+            outtextBox.Text += sb.ToString();
+        }
+    }
+}
